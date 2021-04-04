51918d1665f729a4973955693f5ad14742a43e19
thusharathiwanka <it19076362@my.sliit.lk>
2021-04-04 11:00:13 +0530

    CREATE : location data displayed

--- app/js/app.js
+++ app/js/app.js
@@ -1,9 +1,19 @@
 const API_URL = "https://api.wheretheiss.at/v1/satellites/25544";
 
+const lat = document.querySelector("#lat");
+const long = document.querySelector("#long");
+
 const getSatellite = async () => {
   const response = await fetch(API_URL);
   const satellite = await response.json();
-  console.log(satellite);
+  const { latitude, longitude } = satellite;
+
+  setLocation(latitude, longitude);
+};
+
+const setLocation = (latitude, longitude) => {
+  lat.textContent = latitude;
+  long.textContent = longitude;
 };
 
 getSatellite();
