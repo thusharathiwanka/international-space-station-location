589911fb5af2f6a87afa89add1ebec44ddf5cd5c
thusharathiwanka <it19076362@my.sliit.lk>
2021-04-04 12:20:29 +0530

    CREATE : map added

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,11 @@
 const API_URL = "https://api.wheretheiss.at/v1/satellites/25544";
+const MAP = L.map("map").setView([0, 0], 1.5);
+const attribution =
+  '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+
+const tileUrl = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
+const tiles = L.tileLayer(tileUrl, { attribution });
+tiles.addTo(MAP);
 
 const lat = document.querySelector("#lat");
 const long = document.querySelector("#long");
