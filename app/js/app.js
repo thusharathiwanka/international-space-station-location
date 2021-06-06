4d28957929223ce883ce30d65e5e0794d2f5cdcc
Thushara Thiwanka <thusharathiwanka123@gmail.com>
2021-06-07 03:12:33 +0530

    UPDATE: api keys seperated

--- app/js/app.js
+++ app/js/app.js
@@ -1,11 +1,11 @@
-const API_URL = "https://api.wheretheiss.at/v1/satellites/25544";
+import { API_URL } from "./config.js";
 // creating a map
 const MAP = L.map("map").setView([0, 0], 1.5);
 // adding marker to the map
 const marker = L.marker([0, 0]).addTo(MAP);
 // adding copyrights
 const attribution =
-  '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+	'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
 // adding tile image to the map
 const tileUrl = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
 const tiles = L.tileLayer(tileUrl, { attribution });
@@ -18,27 +18,27 @@ const long = document.querySelector("#long");
 
 // fetching data from api
 const getSatellite = async () => {
-  const response = await fetch(API_URL);
-  const satellite = await response.json();
-  const { latitude, longitude } = satellite;
+	const response = await fetch(API_URL);
+	const satellite = await response.json();
+	const { latitude, longitude } = satellite;
 
-  // setting latitude and longitude
-  setLocation(latitude, longitude);
+	// setting latitude and longitude
+	setLocation(latitude, longitude);
 };
 
 // set location in text and map
 const setLocation = (latitude, longitude) => {
-  // setting latitude and longitude as text
-  lat.textContent = latitude.toFixed(2);
-  long.textContent = longitude.toFixed(2);
+	// setting latitude and longitude as text
+	lat.textContent = latitude.toFixed(2);
+	long.textContent = longitude.toFixed(2);
 
-  // updating marker
-  marker.setLatLng([latitude, longitude]);
-  // updating map and zoom
-  if (initialLoad) {
-    MAP.setView([latitude, longitude], 3);
-    initialLoad = false;
-  }
+	// updating marker
+	marker.setLatLng([latitude, longitude]);
+	// updating map and zoom
+	if (initialLoad) {
+		MAP.setView([latitude, longitude], 3);
+		initialLoad = false;
+	}
 };
 
 // initial call
---
+++ app/js/config.js
@@ -0,0 +1,1 @@
+export const API_URL = "https://api.wheretheiss.at/v1/satellites/25544";
